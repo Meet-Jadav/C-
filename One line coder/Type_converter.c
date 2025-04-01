54fd207b549871ab947b0ac7df45e8aad8da9db3
Meet-Jadav <jadavmeet2006@gmail.com>
2025-04-01 15:59:54 +0530

    added bubble sort

---
+++ One line coder/Type_converter.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main (){
+    int n ;
+    printf("Enter your no :");
+    scanf("%d",&n);
+    printf("The decimal value is:%d",n);
+    printf("\nThe Hexadecimal value is:%x",n);
+    printf("\nThe Octal value is:%o",n);
+    // printf("\nThe binary value is:%b",n);  // this is also working .
+    sprintf(Hex,"\n\n%X",n); this works somewhere 
+
+
+    return 0;
+}
--- Sorting/Bubblesort.c
+++ Sorting/Bubblesort.c
@@ -1,7 +1,7 @@
 // program to sort an array .
 #include <stdio.h>
 
-void bubblesort(int arr[],int n){
+void bubblesort(int arr[],int n){   // time : O(n^2);
     for (int i=0;i<n-1;i++){
         for (int j=0;j<n-1-i;j++){
             int temp;
